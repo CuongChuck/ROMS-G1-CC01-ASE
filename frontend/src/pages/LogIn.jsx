0b7849621b804ce9d4bff2479194423604c8acb4
cuongchuck <englishtalent123@gmail.com>
2025-03-24 17:59:45 +0700

    Add navigation behavior to register button

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import './LogIn.css';
 import AuthForm from '../components/AuthForm';
 import SecondaryButton from '../components/SecondaryButton';
+import { useNavigate } from 'react-router-dom';
 
 const LogIn = () => {
+    const navigate = useNavigate();
+
     return (
         <div className='AuthPage'>
             <AuthForm content={1} />
@@ -12,6 +15,7 @@ const LogIn = () => {
                 <SecondaryButton
                     content={0}
                     style={{width:'100%'}}
+                    onClick={() => navigate('/register')}
                 />
             </div>
         </div>
