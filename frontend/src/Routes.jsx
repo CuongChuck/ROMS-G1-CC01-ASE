26dad6c50fd1ddbd354522c98e807daca8e3d584
cuongchuck <englishtalent123@gmail.com>
2025-03-24 17:58:34 +0700

    Add register page

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,9 +1,11 @@
 import Home from "./pages/Home";
 import LogIn from "./pages/LogIn";
+import Register from "./pages/Register";
 
 const routes = [
     {path: '/', component: Home},
-    {path: '/login', component: LogIn}
+    {path: '/login', component: LogIn},
+    {path: '/register', component: Register}
 ];
 
 export { routes };
