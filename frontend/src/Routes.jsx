17e3757eb53f19616a94401467d3e7d7b5c18a90
cuongchuck <englishtalent123@gmail.com>
2025-03-24 12:24:37 +0700

    Add route to login page

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,7 +1,9 @@
 import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
 
 const routes = [
-    {path: '/', component: Home}
+    {path: '/', component: Home},
+    {path: '/login', component: LogIn}
 ];
 
 export { routes };
