acd37359543c132956dd632507fcce21d345d699
cuongchuck <englishtalent123@gmail.com>
2025-03-24 23:29:15 +0700

    Update directories, Add room info page

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,13 +1,15 @@
-import Account from "./pages/Account";
-import Home from "./pages/Home";
-import LogIn from "./pages/LogIn";
-import Register from "./pages/Register";
+import Account from "./pages/Account/Account";
+import Home from "./pages/Home/Home";
+import LogIn from "./pages/LogIn/LogIn";
+import Register from "./pages/Register/Register";
+import Room from "./pages/Room/Room";
 
 const routes = [
     {path: '/', component: Home},
     {path: '/login', component: LogIn},
     {path: '/register', component: Register},
-    {path: '/account', component: Account}
+    {path: '/account', component: Account},
+    {path: '/room', component: Room}
 ];
 
 export { routes };
