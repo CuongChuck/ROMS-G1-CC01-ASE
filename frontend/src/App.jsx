8e7f1d85a8f56639d26a72c410f4bc0f022f084d
cuongchuck <englishtalent123@gmail.com>
2025-03-24 12:24:29 +0700

    Handle backward button of browser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,50 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, useLocation, useNavigate } from "react-router";
 import { routes } from './Routes';
 import NavBar from "./components/NavBar";
+import { useEffect } from "react";
+
+const BackButtonHandler = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const handlePopState = () => {
+            console.log('Back button pressed!');
+            navigate(-1);
+        };
+
+        window.addEventListener('popstate', handlePopState);
+
+        return () => {
+            window.removeEventListener('popstate', handlePopState);
+        };
+    }, [navigate]);
+
+    return null;
+};
+
+const NavBarWrapper = () => {
+    const location = useLocation();
+    let state = '';
+    if (location.pathname === '/') state = 'home';
+    else if (location.pathname === '/room') state = 'room';
+    else if (location.pathname === '/login') state = 'account';
+
+    return <NavBar state={state} />;
+};
 
 const App = () => {
     return (
         <div>
+            <BackButtonHandler />
+            <NavBarWrapper />
             <Routes>
                 {routes.map((route, index) => {
                     const Page = route.component;
-                    let navState = '';
-                    if (index == 0) navState = 'home';
-                    else if (index == 1) navState = 'account';
-                    else if (index == 3) navState = 'account';
-                    else if (index > 3) navState = 'room';
                     return (
                         <Route
                             key={index}
                             path={route.path}
-                            element={<>
-                                <NavBar state={navState} />
-                                <Page />
-                            </>}
+                            element={<Page />}
                         />                
                     );
                 })}
