2c39a902af1ce0213799e1b0295fa87911d36283
cuongchuck <englishtalent123@gmail.com>
2025-03-24 22:39:49 +0700

    Add account page

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,3 +1,4 @@
+import Account from "./pages/Account";
 import Home from "./pages/Home";
 import LogIn from "./pages/LogIn";
 import Register from "./pages/Register";
@@ -5,7 +6,8 @@ import Register from "./pages/Register";
 const routes = [
     {path: '/', component: Home},
     {path: '/login', component: LogIn},
-    {path: '/register', component: Register}
+    {path: '/register', component: Register},
+    {path: '/account', component: Account}
 ];
 
 export { routes };
