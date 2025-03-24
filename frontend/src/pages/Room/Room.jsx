abc21c7d5412b665c6099f813766e5cd85a7ccd8
cuongchuck <englishtalent123@gmail.com>
2025-03-24 23:30:05 +0700

    Add room info page

---
+++ frontend/src/pages/Room/Room.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Table from '../../components/Table/Table';
+
+const Room = () => {
+    return (
+        <Table title={1} />
+    )
+}   
+
+export default Room 
