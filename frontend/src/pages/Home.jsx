cfe1bd2a7adcb93a22dce2297f55d11e50bb143a
cuongchuck <englishtalent123@gmail.com>
2025-03-24 17:59:23 +0700

    Add navigate behavior to register button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,15 +3,18 @@ import room1 from '../assets/Phong1.jpg';
 import room2 from '../assets/Phong2.jpg';
 import './Home.css';
 import PrimaryButton from '../components/PrimaryButton';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate();
+
     return (
         <div>
             <div className='Intro'>
                 <div className='block'>
                     <h1>ĐĂNG KÝ PHÒNG HỌC</h1>
                     <h3>Hệ thống đăng ký phòng học dành cho giảng viên</h3>
-                    <PrimaryButton content={0} />
+                    <PrimaryButton content={0} onClick={() => navigate('/register')} />
                 </div>
                 <img src={room1} alt='Phong hoc 1' width={600} />
             </div>
