0d45ac9ab82b3e0c2916fc49f047797e7c226d50
cuongchuck <englishtalent123@gmail.com>
2025-03-27 09:49:54 +0700

    Add room list page

---
+++ frontend/src/pages/RoomList/RoomList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './RoomList.css';
+import SearchBar from '../../components/SearchBar/SearchBar';
+
+const RoomList = () => {
+    return (
+        <div className='RoomListPage'>
+            <h2>TÌM PHÒNG HỌC</h2>
+            <SearchBar />
+            
+        </div>
+    )
+}
+
+export default RoomList
