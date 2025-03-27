833f5d16fceb76798bd302dd05718efb6b77bda5
cuongchuck <englishtalent123@gmail.com>
2025-03-27 18:07:18 +0700

    Update style of primary button

--- frontend/src/components/PrimButton/PrimaryButton.jsx
+++ frontend/src/components/PrimButton/PrimaryButton.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './PrimaryButton.css';
 
 const PrimaryButton = ({content, onClick}) => {
+    const size = content == 2 ? 'var(--tiny-textsize)' : 'var(--small-textsize)';
     return (
         <div className='primbutton' onClick={onClick}>
-            <h4>{content === 0
+            <h4 style={{fontSize:size}}>{content === 0
                 ? 'Đăng ký'
                 : content === 1
                 ? 'Đăng nhập'
+                : content === 2
+                ? 'Đăng ký'
                 : 'Xác nhận đăng ký'
             }</h4>
         </div>
