6c5128f5e9a27bd15e82ff3884c45ed4d3a04585
cuongchuck <englishtalent123@gmail.com>
2025-03-27 18:08:34 +0700

    Update search result of room list page

--- frontend/src/pages/RoomList/RoomList.jsx
+++ frontend/src/pages/RoomList/RoomList.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './RoomList.css';
 import SearchBar from '../../components/SearchBar/SearchBar';
+import Room from '../../components/RoomInfo/RoomInfo';
 
 const RoomList = () => {
     return (
         <div className='RoomListPage'>
             <h2>TÌM PHÒNG HỌC</h2>
             <SearchBar />
-            
+            <Room state={0} />
         </div>
     )
 }
