ed523b49fd7eae2499ffa9b57c8646cb7eaab11c
cuongchuck <englishtalent123@gmail.com>
2025-03-27 18:08:04 +0700

    Update style of secondary button

--- frontend/src/components/SecButton/SecondaryButton.jsx
+++ frontend/src/components/SecButton/SecondaryButton.jsx
@@ -3,10 +3,11 @@ import './SecondaryButton.css';
 
 const SecondaryButton = ({content, style, onClick}) => {
     const lst = ['Đăng ký', 'Thông tin tài khoản', 'Xem lịch sử dụng phòng', 'Huỷ'];
+    const size = content == 2 ? 'var(--tiny-textsize)' : 'var(--small-textsize)';
 
     return (
         <div className='secbutton' style={style} onClick={onClick}>
-            <h4>{lst[content]}</h4>
+            <h4 style={{fontSize:size}}>{lst[content]}</h4>
         </div>
     )
 }
