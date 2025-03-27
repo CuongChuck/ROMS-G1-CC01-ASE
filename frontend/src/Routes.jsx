00f05f95addfb766854e9a7035aa437804489d10
cuongchuck <englishtalent123@gmail.com>
2025-03-27 09:49:12 +0700

    Add room list page

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -3,13 +3,15 @@ import Home from "./pages/Home/Home";
 import LogIn from "./pages/LogIn/LogIn";
 import Register from "./pages/Register/Register";
 import Room from "./pages/Room/Room";
+import RoomList from "./pages/RoomList/RoomList";
 
 const routes = [
     {path: '/', component: Home},
     {path: '/login', component: LogIn},
     {path: '/register', component: Register},
     {path: '/account', component: Account},
-    {path: '/room', component: Room}
+    {path: '/room', component: Room},
+    {path: '/rooms', component: RoomList}
 ];
 
 export { routes };
