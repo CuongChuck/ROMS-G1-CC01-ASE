48506bcaa9c8de73899b248b1f701d9057fe0987
cuongchuck <englishtalent123@gmail.com>
2025-04-19 23:24:22 +0700

    Use direct values instead of env

--- backend/config.js
+++ backend/config.js
@@ -3,9 +3,9 @@ import mysql from 'mysql2';
 export const PORT = 8080;
 
 export const mysqlConnection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: "localhost",
+    port: 3306,
+    user: "ROMS_Admin",
+    password: "rAtrObitaNTo",
+    database: "roms",
 });
