b105ff360011c6b057ca40f8912bed23402f3b8e
cuongchuck <englishtalent123@gmail.com>
2025-04-22 00:52:50 +0700

    Add 1 more column to table

--- frontend/src/components/Table/Table.jsx
+++ frontend/src/components/Table/Table.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import './Table.css';
 
-const Table = ({title}) => {
+const Table = () => {
     const room = 'B1-202';
 
     return (
         <div className='Table'>
-            <h1>{title === 0 ? 'LỊCH SỬ ĐĂNG KÝ PHÒNG' : `LỊCH SỬ DỤNG PHÒNG ${room}`}</h1>
+            <h1>LỊCH SỬ ĐĂNG KÝ PHÒNG {room}</h1>
             <table>
                 <tr className='Heading'>
-                    <th>{title === 0 ? 'Phòng' : 'Giảng viên'}</th>
+                    <th>Giảng viên</th>
+                    <th>Môn học</th>
                     <th>Ngày</th>
                     <th>Tiết</th>
                 </tr>
                 <tr>
                     <td>B1-201</td>
+                    <td>DSA</td>
                     <td>01/01/2025</td>
                     <td>2-4</td>
                 </tr>
