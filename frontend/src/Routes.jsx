b27084f6eef212ed7137473a1fd3d3987c385744
cuongchuck <englishtalent123@gmail.com>
2025-04-22 00:50:07 +0700

    Alter order of routes

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,4 +1,5 @@
 import Account from "./pages/Account/Account";
+import F2A from "./pages/F2A/F2A";
 import Home from "./pages/Home/Home";
 import LogIn from "./pages/LogIn/LogIn";
 import Register from "./pages/Register/Register";
@@ -8,11 +9,12 @@ import RoomRegister from "./pages/RoomRegister/RoomRegister";
 
 const routes = [
     {path: '/', component: Home},
-    {path: '/login', component: LogIn},
-    {path: '/register', component: Register},
-    {path: '/account', component: Account},
     {path: '/room', component: Room},
     {path: '/rooms', component: RoomList},
+    {path: '/login', component: LogIn},
+    {path: '/f2a', component: F2A},
+    {path: '/register', component: Register},
+    {path: '/account', component: Account},
     {path: '/room/register', component: RoomRegister}
 ];
 
