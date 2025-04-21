dad701ddc868d919fac2b302b67f612e0b08e55e
cuongchuck <englishtalent123@gmail.com>
2025-04-22 00:52:19 +0700

    Add more info to room info

--- frontend/src/components/RoomInfo/RoomInfo.jsx
+++ frontend/src/components/RoomInfo/RoomInfo.jsx
@@ -5,12 +5,17 @@ import PrimaryButton from '../PrimButton/PrimaryButton';
 import PlaceIcon from '@mui/icons-material/Place';
 import { Link, useNavigate } from 'react-router-dom';
 
-const RoomInfo = ({state}) => {
+const RoomInfo = ({state, account}) => {
     const navigate = useNavigate();
     const color = state == '0' ? 'red' : 'green';
     const room = 'B1-201';
     const fal = 'EE';
     const stateName = state == 0 ? 'Đã dùng' : 'Trống';
+    const date = '01/01/2025';
+    const start = 1;
+    const end = 3;
+    const primButton = account == true ? 4 : 2;
+    const secButton = account == true ? 3 : 2;
     return (
         <div className='Room'>
             <div className='container'>
@@ -21,14 +26,22 @@ const RoomInfo = ({state}) => {
                 <div className='info'>
                     <h5 className='roomNum'>{room}</h5>
                     <h5 className='fal'>{fal}</h5>
-                    <h5>Trạng thái: {stateName}</h5>
+                    {account == true ? (
+                        <h5>Ngày: {date}, Tiết: {start} - {end}</h5>
+                    ) : (
+                        <h5>Trạng thái: {stateName}</h5>
+                    )}
                 </div>
             </div>
             <div className='buttons'>
-                <PrimaryButton content={2} onClick={() => navigate('/room/register')} />
+                <PrimaryButton content={primButton} onClick={() => {
+                    if (account == false) navigate('/room/register');
+                    else navigate('/room/adjust')
+                }
+                } />
                 <span />
                 <Link to='/room' target='_blank' style={{textDecoration:'none'}}>
-                    <SecondaryButton content={2} />
+                    <SecondaryButton content={secButton} />
                 </Link>
             </div>
         </div>
