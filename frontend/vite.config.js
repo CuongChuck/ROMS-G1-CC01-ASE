ae8d5bb4168dd61b5683bdbdc9091a3aec957f64
cuongchuck <englishtalent123@gmail.com>
2025-04-22 20:02:50 +0700

    Add configuration for https

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [react()],
+    server: {
+        https: {
+            key: fs.readFileSync(path.resolve('../server.key')),
+            cert: fs.readFileSync(path.resolve('../server.crt')),
+        },
+        host: true, // Listen on all public IPs
+    },
 })
