25b44ddbda5f48a4f543e373fa995a748a3eee8d
cuongchuck <englishtalent123@gmail.com>
2025-04-28 22:15:24 +0700

    Add more functions to profile page

--- frontend/src/pages/Account/Account.jsx
+++ frontend/src/pages/Account/Account.jsx
@@ -5,16 +5,19 @@ import Room from '../../components/RoomInfo/RoomInfo';
 import PrimaryButton from '../../components/PrimButton/PrimaryButton';
 import axios from 'axios';
 import useAuth from '../../content/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const Account = () => {
     const [name, setName] = useState('');
     const [fal_name, setFalName] = useState('');
+    const [role, setRole] = useState(0);
     axios.defaults.withCredentials = true;
     const { handleLogout } = useAuth();
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios
-            .get('http://localhost:8080/user', {
+            .get('https://localhost:8080/profile', {
                 headers: {
                     "access-token": localStorage.getItem("token")
                 }
@@ -22,6 +25,7 @@ const Account = () => {
             .then((response) => {
                 setName(response.data.name);
                 setFalName(response.data.faculty);
+                setRole(response.data.role);
             })
             .catch(() => {
                 alert("An error occurred while retrieving user info");
@@ -32,13 +36,13 @@ const Account = () => {
         <div className='AccountPage'>
             <div className='Info'>
                 <div className='info'>
-                    <h3 className='Firstname'>{name}</h3>
+                    <h3 className='Firstname'>{role == 1 ? 'Giảng viên: ' : ''}{name}</h3>
                     <h3>{fal_name}</h3>
                 </div>
                 <div className='accountButtons'>
-                    <PrimaryButton content={6} />
+                    <PrimaryButton content={6} onClick={() => navigate('/profile')} />
                     <span style={{marginBottom:'10px'}} />
-                    <SecondaryButton content={1} onClick={handleLogout} />
+                    <SecondaryButton content={1} onClick={() => handleLogout()} />
                 </div>
             </div>
             <h2 style={{margin:'80px 0px 50px'}}>LỊCH SỬ ĐĂNG KÝ PHÒNG</h2>
