0292390a04b156343bd58063d520e303c217abb0
cuongchuck <englishtalent123@gmail.com>
2025-04-28 22:14:58 +0700

    Add more content to button

--- frontend/src/components/PrimButton/PrimaryButton.jsx
+++ frontend/src/components/PrimButton/PrimaryButton.jsx
@@ -3,7 +3,7 @@ import './PrimaryButton.css';
 
 const PrimaryButton = ({content, onClick}) => {
     const size = content == 2 ? 'var(--tiny-textsize)' : 'var(--small-textsize)';
-    const info = ['Đăng ký', 'Đăng nhập', 'Đăng ký', 'Xác nhận đăng ký', 'Chỉnh sửa', 'Xác nhận', 'Thông tin tài khoản'];
+    const info = ['Đăng ký', 'Đăng nhập', 'Đăng ký', 'Xác nhận đăng ký', 'Chỉnh sửa', 'Xác nhận', 'Thông tin tài khoản', 'Chỉnh sửa tài khoản'];
     return (
         <div className='primbutton' onClick={onClick}>
             <h4 style={{fontSize:size}}>{info[content]}</h4>
--- frontend/src/components/SecButton/SecondaryButton.jsx
+++ frontend/src/components/SecButton/SecondaryButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './SecondaryButton.css';
 
 const SecondaryButton = ({content, style, onClick}) => {
-    const lst = ['Đăng ký', 'Đăng xuất', 'Xem lịch sử dụng phòng', 'Huỷ', 'Gửi lại mã'];
+    const lst = ['Đăng ký', 'Đăng xuất', 'Xem lịch sử dụng phòng', 'Huỷ', 'Gửi lại mã', 'Xoá tài khoản'];
     const size = content == 2 ? 'var(--tiny-textsize)' : 'var(--small-textsize)';
 
     return (
