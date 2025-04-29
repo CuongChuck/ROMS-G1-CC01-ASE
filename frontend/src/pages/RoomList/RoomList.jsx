db2d298462fde03976a2fe7db990e6256570d81c
cuongchuck <englishtalent123@gmail.com>
2025-04-30 00:12:47 +0700

    Get room list

--- frontend/src/pages/RoomList/RoomList.jsx
+++ frontend/src/pages/RoomList/RoomList.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './RoomList.css';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import Room from '../../components/RoomInfo/RoomInfo';
+import axios from 'axios';
 
 const RoomList = () => {
+    const [roomList, setRoomList] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("https://localhost:8080/room/list")
+            .then((response) => {
+                setRoomList(response.data.data);
+            })
+            .catch(() => {
+                alert("Error occurred when retrieving room list");
+            })
+    }, []);
+
     return (
         <div className='RoomListPage'>
             <h2>TÌM PHÒNG HỌC</h2>
             <SearchBar />
-            <Room state={0} account={false} />
+            {roomList.map((room) => {
+                return (
+                    <Room
+                        building={room.BuildingName}
+                        num={room.RoomNum}
+                        faculty={room.FacultyName}
+                        add={room.Location}
+                        id={room.RoomID}
+                        account={false}
+                    />
+                );
+            })}
         </div>
     )
 }
